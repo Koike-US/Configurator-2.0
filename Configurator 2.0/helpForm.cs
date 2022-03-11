5f0aa8fd88085bab0eb16abf6809a18a76f50e78
nsinno84 <nsinno84@gmail.com>
2022-03-11 14:17:12 -0500

    master update

---
+++ Configurator 2.0/helpForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Configurator_2._0
+{
+    public partial class helpForm : Form
+    {
+        public helpForm()
+        {
+            InitializeComponent();
+            configMachList.DataSource = Globals.machineData;
+            configMachList.DisplayMember = "ModelCombo";
+        }
+
+        private void okButt_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
